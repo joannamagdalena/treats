936ce1268c99dbabbafded6bd3352d74d464ccd8
Joanna <joannakaczmarek93@gmail.com>
2024-11-16 12:35:31 +0100

    choosing images for removal

--- background removal.py
+++ background removal.py
@@ -3,10 +3,16 @@ from rembg import remove
 from PIL import Image
 
 def choose_images_to_background_removal():
-    training_data_path = "../training_data/"
+    training_data_path = "../training_data"
     dataset_files = glob.glob(training_data_path+"/*/*/*")
-    prepared_training_data_path = "../training_data_without_background/"
+    prepared_training_data_path = "../training_data_without_background"
     prepared_dataset_files = glob.glob(prepared_training_data_path+"/*/*/*")
+
+    training_files_diff = [f[len(training_data_path):] for f in dataset_files
+                           if prepared_training_data_path+f[len(training_data_path):] not in prepared_dataset_files]
+
+    return training_files_diff
+
 
 
 choose_images_to_background_removal()
