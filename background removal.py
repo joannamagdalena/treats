6d150acdb37b1d9b9284f8745198ccdab1c29848
Joanna <joannakaczmarek93@gmail.com>
2024-11-24 13:49:30 +0100

    background removal for training data

--- background removal.py
+++ background removal.py
@@ -13,12 +13,14 @@ def choose_images_to_background_removal():
 
     return training_files_diff
 
-def background_removal(image_path):
-    im = Image.open("../training_data"+image_path)
-    image_without_background = remove(im)
-    image_without_background_converted = image_without_background.convert("RGB")
-    image_without_background_converted.save( "../training_data_without_background"+image_path)
+def background_removal_from_image_training_data():
+    images_for_background_removal = choose_images_to_background_removal()
+    for image_path in images_for_background_removal:
+        im = Image.open("../training_data"+image_path)
+        image_without_background = remove(im)
+        image_without_background_converted = image_without_background.convert("RGB")
+        image_without_background_converted.save( "../training_data_without_background"+image_path)
 
 
+#def background_removal(given_image_path):
 
-#background_removal(choose_images_to_background_removal()[0])
